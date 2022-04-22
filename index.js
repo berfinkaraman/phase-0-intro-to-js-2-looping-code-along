ffe3ca21992755f755831af94e0632893f4b62e5
Berfin <berfin@minerva.kgi.edu>
2022-04-22 16:27:50 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+const stringNames = ["Guadalupe", "Ollie", "Aki"];
+const eventName = "surprise";
+
+function writeCards(stringNames, eventName) {
+  let cards = [];
+
+  
+  for (let i = 0; i < stringNames.length; i++) {
+    cards.push(`Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return cards;
+}
+
+
+
+
+function countDown(integer) {
+    while (integer > 0) {
+        console.log(integer);
+        integer--;
+    }
+    console.log(integer);
+}
